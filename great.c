df328be8f3bd2530ae1a4d87e4d0179ac9b12707
clintonantony <clintonantonyagm@gmail.com>
2017-09-02 12:51:03 +0530

    Create great.c

---
+++ great.c
@@ -0,0 +1,12 @@
+void main()
+{
+ int a,b,c;
+ scanf("%d %d %d",&a,&b,&c);
+ if(a>b && a>c)
+  printf("%d is greater",a);
+ else
+  if(b>c)
+    printf("%d is greater",b);
+ else{
+   printf("%d is greater",c)';
+}   
