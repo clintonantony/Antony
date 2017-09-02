06531bcaea2c00e01dd83e8ca43c8464e0974b82
clintonantony <clintonantonyagm@gmail.com>
2017-09-02 11:11:44 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a;
+ scanf("%d",&a);
+ if(a>0)
+ {
+  printf("%d is positive",a);
+  }
+  else
+    if(a<0)
+     { 
+      printf("%d is negative",a);
+      } 
+     else
+     {
+     printf("number is zero");
+     }
+ }    
