e31583603e50de6ef7f3a2e63700dfb65cdf2a9a
clintonantony <clintonantonyagm@gmail.com>
2017-09-02 13:59:48 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -2,10 +2,17 @@ void main()
 {
  int N,s;
  scanf("%d",&N);
- while(N!=0)
+ if(N>0)
  {
-  s+=(N%10);
-  N=N/10;
-  }
-  printf("sum of N is %d",s);
-}
+  while(N!=0)
+  {
+   s+=(N%10);
+   N=N/10;
+   }
+   printf("sum of N is %d",s);
+ }
+ else
+ {
+  printf("give correct input");
+ }
+} 
