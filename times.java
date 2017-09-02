a9df0069d78cdd7fb2ac770f7236297a045a64dd
clintonantony <clintonantonyagm@gmail.com>
2017-09-02 12:34:33 +0530

    Update and rename times.py to times.java

---
+++ times.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class repeat
+ { 
+  public static void main(String[] args)
+  {
+   
+    int i;
+    String s="hello";
+    for(i=0;i<5;i++)
+        System.out.println(s);
+   }
+ }  
